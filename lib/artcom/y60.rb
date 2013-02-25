f4b17e281dcfef7034547517f887aba194022e01
Gunnar Marten <gunnar.marten@artcom.de>
2013-02-25 21:06:20 +0100

    removed deploy namespace

--- lib/artcom/y60.rb
+++ lib/artcom/y60.rb
@@ -9,35 +9,33 @@ configuration = Capistrano::Configuration.respond_to?(:instance) ?
 
 configuration.load do
   # --------------------------------------------
-  # Task chains
+  # Task hooks
   # --------------------------------------------
-  after "deploy:setup", "y60:deploy:update_ldconfig"
-  after "deploy:setup", "y60:deploy:update_environment"
+  after "deploy:setup", "y60:update_ldconfig"
+  after "deploy:setup", "y60:update_environment"
 
   # --------------------------------------------
   # y60 specific tasks
   # --------------------------------------------
   namespace :y60 do
-    namespace :deploy do
 
-      desc "Add asl/lib & y60/lib to ldconfig"
-      task :update_ldconfig, :roles => :app do
-        run "echo '#{shared_path}/asl/lib' | #{sudo} tee /etc/ld.so.conf.d/asl.conf", :pty => true
-        run "echo '#{shared_path}/y60/lib' | #{sudo} tee /etc/ld.so.conf.d/y60.conf", :pty => true
-        run "#{sudo} /sbin/ldconfig", :pty => true
-      end
+    desc "Add asl/lib & y60/lib to ldconfig"
+    task :update_ldconfig, :roles => :app do
+      run "echo '#{shared_path}/asl/lib' | #{sudo} tee /etc/ld.so.conf.d/asl.conf", :pty => true
+      run "echo '#{shared_path}/y60/lib' | #{sudo} tee /etc/ld.so.conf.d/y60.conf", :pty => true
+      run "#{sudo} /sbin/ldconfig", :pty => true
+    end
 
-      desc "Setup environment variable y60 'Y60_DIR"
-      task :update_environment, :roles => :app do
-        next if find_servers_for_task(current_task).empty?
-        run "echo 'export Y60_DIR=#{shared_path}/y60/bin' | #{sudo} tee /etc/profile.d/y60.sh", :pty => true
-      end
+    desc "Setup environment variable y60 'Y60_DIR'"
+    task :update_environment, :roles => :app do
+      next if find_servers_for_task(current_task).empty?
+      run "echo 'export Y60_DIR=#{shared_path}/y60/bin' | #{sudo} tee /etc/profile.d/y60.sh", :pty => true
+    end
 
-      desc "Copy Y60 engine including asl, watchdog"
-      task :copy_binary, :roles => :app do
-        top.upload("y60.tar.gz", "#{shared_path}", :via=> :scp)
-        run "tar -C '#{shared_path}' -xzvf '#{shared_path}/y60.tar.gz'"
-      end
+    desc "Copy Y60 engine including asl, watchdog"
+    task :copy_binary, :roles => :app do
+      top.upload("y60.tar.gz", "#{shared_path}", :via=> :scp)
+      run "tar -C '#{shared_path}' -xzvf '#{shared_path}/y60.tar.gz'"
     end
   end
 end

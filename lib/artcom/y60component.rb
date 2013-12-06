0755166f0fa141f6f4940ea8caf4982afa1ca098
gunnar <gunnar.marten@artcom.de>
2013-12-06 10:55:38 +0100

    fixed y60 component deployment script

--- lib/artcom/y60component.rb
+++ lib/artcom/y60component.rb
@@ -11,10 +11,9 @@ configuration.load do
   # --------------------------------------------
   # Task hooks
   # --------------------------------------------
-  after "deploy:setup", "y60component:setup_directory_structure" if :y60_components.any?
-  after "deploy:setup", "y60component:update_ldconfig" if :y60_components.any?
-
-  after "deploy:setup", "y60component:update_environment" if :y60_components.any?
+  after "deploy:setup", "y60component:setup_directory_structure"
+  after "deploy:setup", "y60component:update_ldconfig"
+  after "deploy:setup", "y60component:update_environment"
 
 
   # --------------------------------------------
@@ -23,13 +22,13 @@ configuration.load do
   namespace :y60component do
     desc "setup directory structure"
     task :setup_directory_structure, :roles => :app do
-      :y60_components.each {|c|
+      y60_components.each {|c|
         run "mkdir -p #{deploy_to}/../install/#{c}"
       }
     end
     desc "Add component to ldconfig"
     task :update_ldconfig, :roles => :app do
-      :y60_components.each {|c|
+      y60_components.each {|c|
         run "echo '#{deploy_to}/../install/#{c}/lib/y60/components' | #{sudo} tee /etc/ld.so.conf.d/#{c}.conf", :pty => true
         run "#{sudo} /sbin/ldconfig", :pty => true
       }
@@ -37,7 +36,7 @@ configuration.load do
 
     desc "Copy component"
     task :copy_plugin, :roles => :app do
-      :y60_components.each {|c|
+      y60_components.each {|c|
         top.upload("#{c}.tar.gz", "#{deploy_to}/../install/", :via=> :scp)
         run "tar -C '#{deploy_to}/../install/' -xzvf '#{deploy_to}/../install/#{c}.tar.gz'"
       }
@@ -45,7 +44,7 @@ configuration.load do
     desc "Set environment variable"
     task :update_environment, :roles => :app do
       next if find_servers_for_task(current_task).empty?
-      :y60_components.each {|c|
+      y60_components.each {|c|
         run "echo 'export #{c.to_s.upcase.gsub( %r{[\W]+}, '' )}_DIR=#{deploy_to}/../install/#{c}/' | #{sudo} tee /etc/profile.d/#{c}.sh", :pty => true
       }
     end
